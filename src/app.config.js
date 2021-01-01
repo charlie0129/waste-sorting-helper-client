2f36b355f417cf2523c9c94dc009e429ad143812
Charlie Chiang <charlie_c_0129@outlook.com>
2021-01-01 16:29:59 +0800

    feat: add navigation bar to the bottom

--- src/app.config.js
+++ src/app.config.js
@@ -7,5 +7,25 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    color: '#666',
+    selectedColor: '#28aaff',
+    backgroundColor: '#fafafa',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/home/home-page',
+        iconPath: './assets/icons/icon_home_n@2x.png',
+        selectedIconPath: './assets/icons/icon_home_s@2x.png',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/home/home-page',
+        iconPath: './assets/icons/icon_me_n@2x.png',
+        selectedIconPath: './assets/icons/icon_me_s@2x.png',
+        text: '我的',
+      },
+    ],
   }
 }
